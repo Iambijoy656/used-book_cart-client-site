b9f2142f8c76de7490cc2ffa8b1fcbda4af0a955
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-11-26 20:08:49 +0600

    authProvider add

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import AuthProvider from './context/AuthProvider';
 
 
 // Create a client
@@ -17,8 +18,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </QueryClientProvider>
+
   </React.StrictMode>
 );
 
