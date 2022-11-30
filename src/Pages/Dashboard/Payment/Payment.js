7757f24163cb50342265af60d41e7c3539a4c708
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-11-30 18:42:18 +0600

    payment done

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import CheckoutForm from "./CheckoutForm";
+
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+
+
+console.log(stripePromise)
 
 const Payment = () => {
     const order = useLoaderData();
@@ -15,9 +24,9 @@ const Payment = () => {
                     Please pay <strong>${price}</strong>
                 </p>
                 <div className="w-96 my-12">
-                    {/* <Elements stripe={stripePromise}>
+                    <Elements stripe={stripePromise}>
                         <CheckoutForm order={order}></CheckoutForm>
-                    </Elements> */}
+                    </Elements>
                 </div>
             </div>
         </>
